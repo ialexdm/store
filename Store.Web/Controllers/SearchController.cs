8061ff8109766441b1628d1ac252324cafa5f2bd
Александр <ivashininad@gmail.com>
2022-08-31 15:06:12 +0300

    Add Sech page

---
+++ Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+
+        private readonly IBookRepository bookRepository;
+        
+        
+        public SearchController(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+
+        // GET: SearchController
+        public ActionResult Index(string query)
+        {
+
+            var books = bookRepository.GetAllByTitle(query);
+
+            return View(books);
+        }
+    }
+}
