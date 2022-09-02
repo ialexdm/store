7122dc57f8ef87c04e2411baa9d94ef06f756454
Александр <ivashininad@gmail.com>
2022-09-02 19:58:00 +0300

    Add book to cart #6

---
+++ domain/Store/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class Cart
+    {
+        public IDictionary<int, int> Items { get; set; } = new Dictionary<int, int>();
+
+        public decimal Amount { get; set; }
+    }
+}
