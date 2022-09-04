f213843fe282ec20de73b442976c37f847433daf
Александр <ivashininad@gmail.com>
2022-09-04 12:51:41 +0300

    simplify code of Order

--- Store.Web/Controllers/OrderController.cs
+++ Store.Web/Controllers/OrderController.cs
@@ -26,12 +26,55 @@ namespace Store.Web.Controllers
             }
             return View("Empty");
         }
+        [HttpPost]
+        public IActionResult AddItem(int bookId, int count)
+        {
+            (Order order, Cart cart) = GetOrCreateOrderAndCart();
+            var book = bookRepository.GetById(bookId);
+            order.AddOrUpdateItem(book, count);
 
-        public IActionResult AddItem(int id)
+            SaveOrderAndCart(order, cart);
+
+            return RedirectToAction("Index", "Book", new { bookId });
+
+        }
+        [HttpPost]
+        public IActionResult RemoveItem(int id)
+        {
+            (Order order, Cart cart) = GetOrCreateOrderAndCart();
+
+            order.RemoveItem(id);
+            SaveOrderAndCart(order,cart);
+
+            return RedirectToAction("Index", "Book", new { id});
+        }
+        [HttpPost]
+        public IActionResult UpdateItem(int bookId, int count)
+        {
+            (Order order, Cart cart) = GetOrCreateOrderAndCart();
+
+            order.GetItem(bookId).Count = count;
+            SaveOrderAndCart(order, cart);
+
+            return RedirectToAction("Index", "Book", new { bookId });
+        }
+
+        private void SaveOrderAndCart(Order order, Cart cart)
+        {
+            orderRepository.Update(order);
+
+            cart.TotalCount = order.TotalCount;
+            cart.TotalPrice = order.TotalPrice;
+
+            HttpContext.Session.Set(cart);
+        }
+
+
+
+        private (Order order, Cart cart) GetOrCreateOrderAndCart()
         {
             Order order;
-            Cart cart;
-            if (HttpContext.Session.TryGetCart(out cart))
+            if (HttpContext.Session.TryGetCart(out Cart cart))
             {
                 order = orderRepository.GetById(cart.OrderId);
             }
@@ -41,16 +84,7 @@ namespace Store.Web.Controllers
                 cart = new Cart(order.Id);
             }
 
-            var book = bookRepository.GetById(id);
-            order.AddItem(book, 1);
-            orderRepository.Update(order);
-
-            cart.TotalCount = order.TotalCount;
-            cart.TotalPrice = order.TotalPrice;
-
-            HttpContext.Session.Set(cart);
-
-            return RedirectToAction("Index", "Book", new { id });
+            return (order, cart);
         }
 
         private OrderModel Map(Order order)
--- domain/Store/OrderItem.cs
+++ domain/Store/OrderItem.cs
@@ -8,21 +8,36 @@ namespace Store
 {
     public class OrderItem
     {
-        public int BookId { get; set; }
+        public int BookId { get; }
+
+        private int count;
+
+        public int Count {
+            get { return count; }
+            set 
+            {
+                ThrowIfInvalidCount(value);
+                
+                count = value;
+
+            } }
         
-        public int Count { get; set; }
-        
-        public decimal Price { get; set; }
+        public decimal Price { get; }
 
         public OrderItem(int bookId, int count, decimal price)
         {
-            if(count <= 0)
-            {
-                throw new ArgumentOutOfRangeException("Count must be greater than zero");
-            }
+            ThrowIfInvalidCount(count);
             BookId = bookId;
             Count = count;
             Price = price;
+        }
+
+        private static void ThrowIfInvalidCount(int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Count must be greater than zero");
+            }
         }
     }
 }
